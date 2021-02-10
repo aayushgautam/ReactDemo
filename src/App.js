5051a8db5bd72c0dbaff0552b7b855a9e952d7f1
aayush <aayushgautam@outlook.com>
2021-02-10 10:23:02 +0530

    Converting function to class and using state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import Comp from "./Comp-one/Comp-one";
 import Person from "./Person/Person";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import react from "react";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1> This is a demo  </h1>
-      <h2> Please check out components one by one </h2>
-      <Comp />
-      <Person name="Aayush" age="23" />
-      <Person name="Gautam" age="23">testing out the child component</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Aayush",
+        age: "23",
+      },
+      {
+        name: "Gautam",
+        age: "24",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1> This is a demo </h1>
+        <h2> Please check out components one by one </h2>
+        <Comp />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[0].age} >
+          testing out the child component
+        </Person>
+      </div>
+    );
+  }
 }
 
 export default App;
