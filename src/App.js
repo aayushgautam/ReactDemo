8ee4ec1ec2ea31bded4e258728572299fb11f69b
aayush <aayushgautam@outlook.com>
2021-02-19 12:47:44 +0530

    Revert "adding conditional render"
    This reverts commit 2bb43c3de7fe2c6f9675530427951a704f78dc4b.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = (props) => {
         age: "24",
       },
     ],
-    showDiv: false,
   });
 
   const switchNameHandler = (newName) => {
@@ -50,39 +49,26 @@ const App = (props) => {
     });
   };
 
-  const toggleButton = () => {
-    setPersonsState({
-      
-    })
-  };
-
   return (
-    <div>
-      <div className="App">
-        <h1> This is a demo </h1>
-        <h2> Please check out components one by one </h2>
-        <Comp />
-        <button onClick={() => switchNameHandler("Random Kappoor")}>
-          TRY ME OUT
-        </button>
-        <button onClick={toggleButton}> TOGGLE DIV </button>
-        { === true ? (
-          <div>
-            <Person
-              name={personsState.persons[0].name}
-              age={personsState.persons[0].age}
-              change={nameChangedHandler}
-            />
-            <Person
-              name={personsState.persons[1].name}
-              age={personsState.persons[1].age}
-              click={() => switchNameHandler("Harsh Beniwal")}
-            >
-              testing out the child component
-            </Person>
-          </div>
-        ) : null}
-      </div>
+    <div className="App">
+      <h1> This is a demo </h1>
+      <h2> Please check out components one by one </h2>
+      <Comp />
+      <button onClick={() => switchNameHandler("Random Kappoor")}>
+        TRY ME OUT
+      </button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+        change={nameChangedHandler}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={() => switchNameHandler("Harsh Beniwal")}
+      >
+        testing out the child component
+      </Person>
     </div>
   );
 };
